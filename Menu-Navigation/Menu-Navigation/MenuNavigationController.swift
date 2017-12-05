232f5515a3bb5ba57d5a66d50027bf7fb6d64daa
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2017-12-05 10:37:16 +0200

    Add iOS project

---
+++ Menu-Navigation/Menu-Navigation/MenuNavigationController.swift
@@ -0,0 +1,25 @@
+//
+//  MenuNavigationController.swift
+//  Menu-Navigation
+//
+//  Created by dvt on 2017/12/05.
+//  Copyright © 2017 dvt. All rights reserved.
+//
+
+import UIKit
+
+class MenuNavigationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
